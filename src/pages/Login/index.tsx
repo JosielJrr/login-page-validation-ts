aa93f015dff9ca35c60685cc6fdde2e21687deee
Josiel Jr. <josieljrr.dev@gmail.com>
2024-07-29 17:45:38 -0300

    Adiciona função onSubmit

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -21,12 +21,17 @@ const Login = () => {
   const {
     control,
     formState: { errors, isValid },
+    handleSubmit,
   } = useForm<IFormLogin>({
     resolver: yupResolver(schema),
     mode: "onChange",
     defaultValues,
     reValidateMode: "onChange",
   });
+
+  const onSubmit = (data: IFormLogin) => {
+    console.log("Form Submitted:", data);
+  };
 
   return (
     <Container>
@@ -49,7 +54,11 @@ const Login = () => {
             errorMessage={errors?.password?.message}
           />
           <Spacing />
-          <Button title="Entrar" disabled={!isValid} />
+          <Button
+            title="Entrar"
+            disabled={!isValid}
+            onClick={handleSubmit(onSubmit)}
+          />
         </Column>
       </LoginContainer>
     </Container>
